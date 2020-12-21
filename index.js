e40ccc2bd63069f7ff6f45eb8b4d906906e2107e
Nick Playfair <nick@playfair.dev>
2020-12-21 21:13:24 +0000

    Filter parts array
    Remove objects that have a part value which matches the rejectedParts list

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const csvFilePath = './mkII.csv'
+const csvFilePath = './tweed57_smt.csv'
 const csv = require('csvtojson')
+
+// Which components should we remove from the BOM?
+const rejectedParts = [
+  'TP1',
+  'TP2',
+  'TP3',
+  'G',
+  'U$1',
+  'J1',
+  'J2',
+  'INPUT'
+]
+
+// Return false if the Part value of the object passed in is in the list to remove
+function isJunk (element) {
+  return !rejectedParts.includes(element.Part)
+}
 
 csv({
   delimiter: ";",
@@ -7,6 +24,8 @@ csv({
   ignoreEmpty: true
 })
   .fromFile(csvFilePath)
-  .then(parts => {
+  .then(jsonObj => {
+    // Create array containing only relevant parts
+    let parts = jsonObj.filter(isJunk)
     console.log(parts)
   })

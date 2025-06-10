20557ccf58e4d473c287691030648e98a6d8483f
Nick Playfair <842413+nplayfair@users.noreply.github.com>
2025-06-10 18:06:32 +0100

    Remove extraneous quotes from CSV so it doesn't break the JSON

--- src/csvToJSON.ts
+++ src/csvToJSON.ts
@@ -1,8 +1,35 @@
+import { CSVParseParam } from '../node_modules/csvtojson/v2/Parameters';
 import { isJunk } from './utils';
 const csv = require('csvtojson');
 
-export function csvToJSON(csvBOM: string): void {
-  csv()
+//CSV Config for EAGLE BOM
+const csvConfig = {
+  delimiter: `;`,
+  // quote: 'off',
+  ignoreEmpty: true,
+  includeColumns: /(Part|Value)/,
+};
+
+export async function csvToJSON(csvBOM: string) {
+  // csv()
+  //   .fromString(csvBOM)
+  //   .then((jsonObj: object) => {
+  //     console.log(jsonObj);
+  //   });
+  const obj: object = await csv({ csvConfig })
+    .preRawData((csvRawData: string) => {
+      return new Promise((resolve, reject) => {
+        var newData = csvRawData.replace('"', '');
+        resolve(newData);
+      });
+    })
+    .fromString(csvBOM)
+    .then((jsonObj: object) => {
+      return jsonObj;
+    });
+}
+export function simpleCsvToJSON(csvBOM: string) {
+  csv({ csvConfig })
     .fromString(csvBOM)
     .then((jsonObj: object) => {
       console.log(jsonObj);

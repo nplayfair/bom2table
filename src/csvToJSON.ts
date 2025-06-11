5378da4fe2a9917e389b760db45d5b1f0436b19f
Nick Playfair <842413+nplayfair@users.noreply.github.com>
2025-06-11 13:26:09 +0100

    Refactor csv to JSON function

--- src/csvToJSON.ts
+++ src/csvToJSON.ts
@@ -5,33 +5,12 @@ const csv = require('csvtojson');
 //CSV Config for EAGLE BOM
 const csvConfig = {
   delimiter: `;`,
-  // quote: 'off',
+  quote: '"',
   ignoreEmpty: true,
   includeColumns: /(Part|Value)/,
 };
 
-export async function csvToJSON(csvBOM: string) {
-  // csv()
-  //   .fromString(csvBOM)
-  //   .then((jsonObj: object) => {
-  //     console.log(jsonObj);
-  //   });
-  const obj: object = await csv({ csvConfig })
-    .preRawData((csvRawData: string) => {
-      return new Promise((resolve, reject) => {
-        var newData = csvRawData.replace('"', '');
-        resolve(newData);
-      });
-    })
-    .fromString(csvBOM)
-    .then((jsonObj: object) => {
-      return jsonObj;
-    });
-}
-export function simpleCsvToJSON(csvBOM: string) {
-  csv({ csvConfig })
-    .fromString(csvBOM)
-    .then((jsonObj: object) => {
-      console.log(jsonObj);
-    });
+export async function getBOM(csvBOM: string) {
+  const bom = await csv(csvConfig).fromString(csvBOM);
+  return bom;
 }
